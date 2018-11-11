e15a0c662b0cdb5984eea62edd91ef75df7d6a89
Zapliin <zapliin16@mail.com>
2018-11-10 23:00:32 -0300

    Sistema de disparos casi listo.
    El jugador ya apunta y dispara y el enemigo ya recibe daño y muere.

---
+++ Assets/Scripts/Bullet/BulletMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+
+    public float speed = 20f;
+    public Rigidbody rb;
+    public int damage = 20;
+    private Vector3 velocity;
+
+	void Start () {
+        float AimVertical = Input.GetAxisRaw("AimVertical");
+        float AimHorizontal = Input.GetAxisRaw("AimHorizontal");
+        velocity = new Vector3(AimHorizontal, 0, AimVertical);
+        rb.velocity = velocity * speed;
+	}
+
+    private void OnTriggerEnter(Collider hitInfo)
+    {
+        ZombieProperties zombie = hitInfo.GetComponent<ZombieProperties>();
+        if (zombie != null)
+        {
+            zombie.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed;
-    public string direction;
+    public Transform firePoint;
     public Animator animator;
+    public GameObject bulletPrefab;
+
+    private Transform t;
+    private Vector3 initialPos;
+    private float firePosX;
+    private float firePosZ;
     private Rigidbody rb;
 
 	void Start () {
         rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+        initialPos = firePoint.position;
     }
 	
 	void Update () {
         float moveVertical = Input.GetAxis("Vertical");
         float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float AimVertical = Input.GetAxisRaw("AimVertical");
+        float AimHorizontal = Input.GetAxisRaw("AimHorizontal");
 
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         rb.velocity = movement * speed;
@@ -24,25 +35,25 @@ public class PlayerMovement : MonoBehaviour {
         animator.SetFloat("VerticalSpeed", moveVertical);
         animator.SetFloat("HorizontalSpeed", moveHorizontal);
 
-        if (moveHorizontal > 0)
-        {   
-            if (direction != "Right")
-            {
-                //Flip();
-                direction = "Right";
-            }
-        } else if (moveHorizontal < 0)
+        firePoint.position = new Vector3(firePosX, firePoint.position.y, firePosZ);
+
+        if (AimHorizontal != 0)
         {
-            if (direction != "Left")
-            {
-                //Flip();
-                direction = "Left";
-            }
+            firePosX = t.position.x + initialPos.x * AimHorizontal;
+            firePosZ = t.position.z + initialPos.z * AimHorizontal;
+            Shoot();
+        }
+
+        if (AimVertical != 0)
+        {
+            firePosX = t.position.x + initialPos.z * AimVertical;
+            firePosZ = t.position.z + initialPos.x * AimVertical;
+            Shoot();
         }
     }
 
-    private void Flip()
+    private void Shoot()
     {
-        transform.Rotate(0f, 180f, 0f);
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
