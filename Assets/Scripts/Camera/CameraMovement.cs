d5728cb55224cdc19a224cb96e825bd65f4ded2d
Reginorkk <lucascifuentes1@hotmail.com>
2018-12-06 11:02:05 -0300

    mas cambios
    *Cambio en la camara para que siga al personaje en el eje Y.
    
    *Cambio en el box collider del personaje para que no obstruya los disparos.

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -7,16 +7,18 @@ public class CameraMovement : MonoBehaviour {
     public Transform playerTransform;
 
     private float startPosition;
+    private float subir;
     private Transform t;
 
 	void Start () {
         t = GetComponent<Transform>();
         startPosition = t.position.z;
+        subir = t.position.y; //manera de hacer que la camara siga en el eje Y al personaje
     }
 	
 	void Update () {
 
-        Vector3 position = new Vector3(playerTransform.position.x, t.position.y, playerTransform.position.z + startPosition);
+        Vector3 position = new Vector3(playerTransform.position.x, playerTransform.position.y + subir, playerTransform.position.z + startPosition);
         t.position = position;
 	}
 }
